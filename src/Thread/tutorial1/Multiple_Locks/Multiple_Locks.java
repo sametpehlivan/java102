d4d3c630748e01ec8071060215394142e1260ba3
samet <cakal06100@gmail.com>
2023-03-18 19:50:00 +0300

    Multiple Locks

---
+++ src/Thread/tutorial1/Multiple_Locks/Multiple_Locks.java
@@ -0,0 +1,75 @@
+package Thread.tutorial1.Multiple_Locks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class Multiple_Locks
+{
+    private List<Integer> listOne = new ArrayList<>();
+    private List<Integer> listTwo = new ArrayList<>();
+    private Object LOCK1 = new Object();
+    private Object LOCK2 = new Object();
+    public  void addListOne(int i)
+    {
+        try {
+            Thread.sleep(1);
+        }catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        synchronized (LOCK1){
+            listOne.add(i);
+        }
+
+    }
+    public void addListTwo(int i)
+    {
+        try {
+            Thread.sleep(1);
+        }catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        synchronized (LOCK2){
+            listTwo.add(i);
+        }
+
+    }
+    public void addLists()
+    {
+        Thread t1 = new Thread(()->{
+            for (int i=0; i<500; i++)
+            {
+                addListOne(i);
+                addListTwo(i);
+            }
+        });
+        Thread t2 = new Thread(()->{
+            for (int i=0; i<500; i++)
+            {
+                addListOne(i);
+                addListTwo(i);
+            }
+        });
+        t1.start();
+        t2.start();
+        try
+        {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void main()
+    {
+        System.out.println("Starting");
+        long start = System.currentTimeMillis();
+        addLists();
+        System.out.println("Finished...");
+        System.out.println("Time: "+(System.currentTimeMillis()-start));
+        System.out.println("List1 Count: "+ listOne.size()+" List2 Count: "+listTwo.size());
+    }
+}
---
+++ src/Thread/tutorial1/Multiple_Locks/Worker.java
@@ -0,0 +1,12 @@
+package Thread.tutorial1.Multiple_Locks;
+
+public class Worker
+{
+    public static void main(String[] args)
+    {
+        Multiple_Locks m = new Multiple_Locks();
+        m.main();
+
+
+    }
+}
